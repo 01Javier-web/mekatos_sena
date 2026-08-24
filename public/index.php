fd441cee9cb012375081cb24b596cb7d33af5fc0
Javier Ortiz <andres13ytoo@gmail.com>
2026-08-24 17:27:38 -0500

    Metodo Connect

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/../config/database.php';
+
+$database = new Database();
+
+$db = $database->connect();
+
+if ($db) {
+    echo "Conexión exitosa a la base de datos.";
+} else {
+    echo "Error al conectar a la base de datos.";
+}
